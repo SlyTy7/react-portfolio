4a4634b8b961daf118bfef0eb0a2c406cc95af35
slyty7 <tyler.j.west.7@gmail.com>
2018-06-29 08:46:46 -0700

    fix target=_blank vulnerability

--- src/SocialButton.js
+++ src/SocialButton.js
@@ -7,14 +7,14 @@ class SocialButton extends Component {
   render() {
     return (
       <Grid item>
-        <a href={this.props.link} target='_blank' style={{ color: '#fff', textDecoration: 'none' }}>
+        <a href={this.props.link} target='_blank' rel="noopener noreferrer" style={{ color: '#fff', textDecoration: 'none' }}>
           {/* ICON */}
-          <Typography align="center" color="primary">      
+          <Typography align="center" color="primary" gutterBottom>      
             <i className={this.props.icon}></i>
           </Typography> 
 
           {/* DESCRIPTION */}
-          <Typography align="center" variant="body2">
+          <Typography align="center" variant="body1" >
             {this.props.name}
           </Typography>
         </a>  
