9cb04b14ca647562f8342f9a38da0a2eb1dc46df
slyty7 <tyler.j.west.7@gmail.com>
2018-06-29 13:17:34 -0700

    add content to paper component and remove bottom divider

--- src/About.js
+++ src/About.js
@@ -18,31 +18,32 @@ class About extends Component {
 
             <Divider style={{marginBottom: '30px'}}/>
 
-            <Grid container alignItems="center">
-              {/* PROFILE PICTURE */}
-              <Grid item xs={12} md={4}>
-                <img src={AboutImg} alt='placeholder' style={{margin: '0px auto', display: 'block', borderRadius: '50%'}}/>
+            <Paper style={{maxWidth: '700px', margin: '25px auto', padding: '25px'}} >
+
+              <Grid container alignItems="center">
+
+                {/* PROFILE PICTURE */}
+                <Grid item xs={12} md={4}>
+                  <img src={AboutImg} alt='placeholder' style={{margin: '0px auto', display: 'block', borderRadius: '50%'}}/>
+                </Grid>
+
+                {/* ABOUT INFO */}
+                <Grid item xs={12} md={8} style={{ padding: '30px' }}>
+                  <Typography variant="body1" component='p' gutterBottom>
+                    I'm a front-end web developer who lives and works in San Francisco and specializes in creating fully responsive websites and applications. With a background in marketing and web development, I strive to build innovative solutions with technology.
+                  </Typography>
+                  <br />
+                  <Typography variant="body1" component='p'>
+                    Are you looking for a front-end developer? Take a look at some of my work and send me a message. I'm always open to new opportunities!
+                  </Typography>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <SocialButtons />
+                </Grid>
               </Grid>
 
-              {/* ABOUT INFO */}
-              <Grid item xs={12} md={8} style={{ padding: '30px' }}>
-                <Typography variant="body1" component='p' gutterBottom>
-                  I'm a front-end web developer who lives and works in San Francisco and specializes in creating fully responsive websites and applications. With a background in marketing and web development, I strive to build innovative solutions with technology.
-                </Typography>
-                <br />
-                <Typography variant="body1" component='p'>
-                  Are you looking for a front-end developer? Take a look at some of my work and send me a message. I'm always open to new opportunities!
-                </Typography>
-              </Grid>
-
-              <Grid item xs={12}>
-                <SocialButtons />
-              </Grid>
-            </Grid>
-
-          
-
-            <Divider style={{margin: '30px 0px'}}/>
+            </Paper>
 
           </Paper>
         </Grid>
