0a369b0bbc63b4e6bbcae4672404023824591132
slyty7 <tyler.j.west.7@gmail.com>
2018-06-29 08:46:27 -0700

    change font styling

--- src/About.js
+++ src/About.js
@@ -23,11 +23,11 @@ class About extends Component {
                 <img src={AboutImg} alt='placeholder' style={{margin: '0px auto', display: 'block', borderRadius: '50%'}}/>
               </Grid>
               <Grid item xs={12} md={8} style={{ padding: '0px 20px' }}>
-                <Typography variant="body2" component='p' gutterBottom>
+                <Typography variant="body1" component='p' gutterBottom>
                   I'm a front-end web developer who lives and works in San Francisco and specializes in creating fully responsive websites and applications. With a background in marketing and web development, I strive to build innovative solutions with technology.
                 </Typography>
                 <br />
-                <Typography variant="body2" component='p'>
+                <Typography variant="body1" component='p'>
                   Are you looking for a front-end developer? Take a look at some of my work and send me a message. I'm always open to new opportunities!
                 </Typography>
               </Grid>
