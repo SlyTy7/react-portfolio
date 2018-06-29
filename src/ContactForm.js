560088dfedfbec9c3c7ceeaa039b8d9f2ff80aac
slyty7 <tyler.j.west.7@gmail.com>
2018-06-29 13:54:41 -0700

    make contact form functional

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import FormControl from '@material-ui/core/FormControl'
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
 
 class ContactForm extends Component {
@@ -9,21 +10,29 @@ class ContactForm extends Component {
     return (
       <Paper style={{maxWidth: '700px', margin: '25px auto', padding: '25px'}} >
 
-        <FormControl fullWidth margin='normal'>
-          <TextField label='Name' />
-        </FormControl>
+        <form action="https://formspree.io/tyler.j.west.7@gmail.com" method="POST" >
+          <FormControl fullWidth margin='normal'>
+            <TextField name='Name' label='Name' />
+          </FormControl>
 
-        <FormControl fullWidth margin='normal'>
-          <TextField label='Email' />
-        </FormControl>
+          <FormControl fullWidth margin='normal'>
+            <TextField name='Email' label='Email' />
+          </FormControl>
 
-        <FormControl fullWidth margin='normal'>
-          <TextField label='Phone Number' />
-        </FormControl>
-        
-        <FormControl fullWidth margin='normal'>
-          <TextField label='Message' />
-        </FormControl>
+          <FormControl fullWidth margin='normal'>
+            <TextField name='Phone' label='Phone Number' />
+          </FormControl>
+          
+          <FormControl fullWidth margin='normal'>
+            <TextField multiline name='Message' label='Message' />
+          </FormControl>
+
+
+
+          <Button variant="contained" color="primary" type='submit' style={{marginTop: '25px'}}>
+            Send
+          </Button>
+        </form>
       
       </Paper>
     );
